1a454667b0bb5a18750ebcfd3aa54c49c8f03600
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-06-13 14:19:18 -0600

    MenuRegistro

---
+++ Laboratorio_Progamacion_Samuel_Jeremy/src/Semana8/GestionJugador.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana8;
+
+/**
+ *
+ * @author unwir
+ */
+public class GestionJugador {
+    private Jugador jugadorActual;
+    private Jugador jugadores[] = new Jugador[5];
+    private static GestionJugador intanciado;
+    
+    
+    public static GestionJugador getInstance(){
+        if(intanciado == null){
+            intanciado = new GestionJugador();
+        }
+        return intanciado;
+    }
+    
+    private Jugador buscarJugador(String nomJ){
+        for(Jugador j : jugadores){
+            if(j != null && j.getPlayerN().equals(nomJ))
+                return j;
+        }
+        return null;
+    }
+
+    public Jugador getJugadorActual() {
+        return jugadorActual;
+    }
+    
+    public void cerrarSesion(){
+        this.jugadorActual = null;
+    }
+    
+    public boolean agregarJugador(String nomJ, String passW){
+        if(buscarJugador(nomJ) == null ){
+            for(Jugador j: jugadores){
+                if(j == null){
+                    j = new Jugador(nomJ, passW);
+                    return true;
+                }
+            }    
+            return false;
+        }
+        return false;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Laboratorio_Progamacion_Samuel_Jeremy/src/Semana8/Jugador.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana8;
+
+/**
+ *
+ * @author unwir
+ */
+public class Jugador {
+    private String playerN;
+    private String passW;
+    private String puntos;
+
+    public Jugador(String playerN, String passW) {
+        this.playerN = playerN;
+        this.passW = passW;
+    }
+
+    public String getPlayerN() {
+        return playerN;
+    }
+
+    public void setPlayerN(String playerN) {
+        this.playerN = playerN;
+    }
+
+    public String getPassW() {
+        return passW;
+    }
+
+    public void setPassW(String passW) {
+        this.passW = passW;
+    }
+    
+   
+    
+}
